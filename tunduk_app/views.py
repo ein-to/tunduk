a7082de5364ffa096dcb9dea283026894341a6cd
ein-to <etoktomambetov@gmail.com>
2021-04-26 09:52:23 +0600

    com2

--- tunduk_app/views.py
+++ tunduk_app/views.py
@@ -7,8 +7,8 @@ def index(request):
 
 def test_request(request):
     Header = {'Content-type': 'application/json; charset=UTF-8'}
-    data = open('./request.xml')
-    response = requests.get('https://31.186.53.85', headers=Header, data=data, cert=('./subsystemName.crt', './subsystemName.key'))
+    data = open('request.xml')
+    response = requests.get('https://31.186.53.85', headers=Header, data=data, cert=('subsystemName.crt', 'subsystemName.key'))
     #response = requests.get('https://api.github.com')
 
     return render(request, 'tunduk_app/response.html', {'response': response.status_code})
